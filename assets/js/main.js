d862d4b40007564acdea5c75cb8115c77037e894
phi <phi1618jp@gmail.com>
2015-10-09 13:21:14 +0900

    fix shortcut

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,15 @@
 window.addEventListener("DOMContentLoaded", function() {
   hljs.initHighlightingOnLoad();
 }, false);
+
+
+document.addEventListener('keydown', function(e) {
+  var ch = String.fromCharCode(e.keyCode);
+
+  if (ch === 'G') {
+    location.href = '/ghost';
+  }
+  if (ch === 'E') {
+    location.href += '/edit';
+  }
+});

20f81d686bd3f1fe27a61fb19f081c8c5d0e07e2
phi <phi1618jp@gmail.com>
2015-10-12 12:02:06 +0900

    fix setup toc

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,46 @@
-window.addEventListener("DOMContentLoaded", function() {
-  hljs.initHighlightingOnLoad();
-}, false);
+;(function() {
+
+  window.addEventListener("DOMContentLoaded", function() {
+    hljs.initHighlightingOnLoad();
+  }, false);
 
 
-document.addEventListener('keydown', function(e) {
-  var ch = String.fromCharCode(e.keyCode);
+  window.addEventListener('load', function() {
+    setupTOC();
+  }, false);
 
-  if (ch === 'G') {
-    location.href = '/ghost';
-  }
-  if (ch === 'E') {
-    location.href += '/edit';
-  }
-});
+  var setupTOC = function() {
+    var tocElement = document.getElementById('toc');
+    if (!tocElement) return ;
+
+    var parent = tocElement.parentNode;
+    var headers = parent.getElementsByTagName('h2');
+    var list = document.createElement('ul');
+    tocElement.appendChild(list);
+
+    Array.prototype.forEach.call(headers, function(header, i) {
+      var li = document.createElement('li');
+      list.appendChild(li);
+      var anchor = document.createElement('a');
+      li.appendChild(anchor);
+      anchor.innerHTML = header.innerHTML;
+
+      header.id = 'post-header-id-' + i;
+      anchor.href = '#' + header.id;
+    });
+
+  };
+
+  document.addEventListener('keydown', function(e) {
+    var ch = String.fromCharCode(e.keyCode);
+
+    if (ch === 'G') {
+      location.href = '/ghost';
+    }
+    if (ch === 'E') {
+      location.href += '/edit';
+    }
+  });
+
+
+})();

f73f6ca290de2f5d61a55ca2fc07efe5fdc146bf
phi <phi1618jp@gmail.com>
2015-10-21 11:26:27 +0900

    use gulp-pleeease

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 
 var gulp = require('gulp');
 var less = require('gulp-less');
+var pleeease = require('gulp-pleeease');
 
 gulp.task('less', function() {
   gulp
     .src(['assets/less/main.less'])
     .pipe(less({
       ieCompat: false,
+    }))
+    .pipe(pleeease({
+      minifier: false,
+      autoprefixer: {"browsers": ["last 2 versions", "Firefox > 20"]}
     }))
     .pipe(gulp.dest('assets/css/'))
     ;
